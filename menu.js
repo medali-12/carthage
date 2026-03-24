76394567dea88e89722d934b9379e857e5b15327
medali-12 <152554809+medali-12@users.noreply.github.com>
2026-03-24 23:06:58 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -7,22 +7,29 @@ import { firebaseConfig } from "./firebase-config.js";
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const menuContainer = document.getElementById("menu-container");
+const menuContainer = document.getElementById("menu");
 
 async function loadMenu() {
-  menuContainer.innerHTML = "";
+  menuContainer.innerHTML = "<p>Chargement...</p>";
 
   const querySnapshot = await getDocs(collection(db, "produits"));
+
+  menuContainer.innerHTML = ""; // vider
+
   querySnapshot.forEach((docSnap) => {
     const p = docSnap.data();
 
     menuContainer.innerHTML += `
       <div class="menu-item">
         <span class="menu-item-name">${p.nom}</span>
-        <span class="menu-item-price">${p.prix} CHF</span>
+        <span class="menu-item-price">${p.prix} DT</span>
       </div>
     `;
   });
+
+  if (menuContainer.innerHTML.trim() === "") {
+    menuContainer.innerHTML = "<p>Aucun produit pour le moment.</p>";
+  }
 }
 
 loadMenu();
