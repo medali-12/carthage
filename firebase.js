78e43d82f7082b5a108534e6da68fe15b4b81886
medali-12 <152554809+medali-12@users.noreply.github.com>
2026-03-24 20:58:53 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+// Configuration Firebase (celle que tu as copiée)
 const firebaseConfig = {
-  apiKey: "TA_CLE_API",
-  authDomain: "TON_PROJET.firebaseapp.com",
-  projectId: "TON_PROJET",
-  storageBucket: "TON_PROJET.appspot.com",
-  messagingSenderId: "TON_ID",
-  appId: "TON_APP_ID"
+  apiKey: "AIzaSyCzmBq6kM8ufzwzxSO6P-fB8AXxXfBaCQ8",
+  authDomain: "carthage-cafe.firebaseapp.com",
+  projectId: "carthage-cafe",
+  storageBucket: "carthage-cafe.firebasestorage.app",
+  messagingSenderId: "701972195657",
+  appId: "1:701972195657:web:7baec9607886e0b97ed298",
+  measurementId: "G-L2X433VK0M"
 };
 
+// Initialiser Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialiser Firestore
 export const db = getFirestore(app);
