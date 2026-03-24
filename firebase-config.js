21d89e686c9b11c8d6eccbeca3fbdc58a1ccf021
medali-12 <152554809+medali-12@users.noreply.github.com>
2026-03-24 22:46:43 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyCzmBq6kM8ufzwzxSO6P-fB8AXxXfBaCQ8",
   authDomain: "carthage-cafe.firebaseapp.com",
   projectId: "carthage-cafe",
-  storageBucket: "carthage-cafe.firebasestorage.app",
+  storageBucket: "carthage-cafe.appspot.com",
   messagingSenderId: "701972195657",
   appId: "1:701972195657:web:7baec9607886e0b97ed298",
   measurementId: "G-L2X433VK0M"
