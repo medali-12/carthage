032cbf04443d125a106b4d92dad6c42e36f010b2
medali-12 <152554809+medali-12@users.noreply.github.com>
2026-03-24 21:33:39 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCzmBq6kM8ufzwzxSO6P-fB8AXxXfBaCQ8",
+  authDomain: "carthage-cafe.firebaseapp.com",
+  projectId: "carthage-cafe",
+  storageBucket: "carthage-cafe.firebasestorage.app",
+  messagingSenderId: "701972195657",
+  appId: "1:701972195657:web:7baec9607886e0b97ed298",
+  measurementId: "G-L2X433VK0M"
+};
