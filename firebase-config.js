e0fe3501e9623f9b7414362b27dd7026905aa446
medali-12 <152554809+medali-12@users.noreply.github.com>
2026-03-25 22:43:28 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 export const firebaseConfig = {
   apiKey: "AIzaSyCzmBq6kM8ufzwzxSO6P-fB8AXxXfBaCQ8",
   authDomain: "carthage-cafe.firebaseapp.com",
@@ -7,3 +10,6 @@ export const firebaseConfig = {
   appId: "1:701972195657:web:7baec9607886e0b97ed298",
   measurementId: "G-L2X433VK0M"
 };
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
